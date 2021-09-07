9a5f3227f831b0ec1bac944377516bedf8e4a7b6
Caio Fortes <caio.mfortes@gmail.com>
2021-09-07 16:36:05 -0300

    Add files via upload

---
+++ 1semana/AULA 2/AULA 2.py
@@ -0,0 +1,11 @@
+num = int(input("Digite um número: "))
+
+s = num+2
+sb=num-2
+m = num * 2
+div = num/2
+e = num**2
+i = num//2
+
+print(s,"\n", sb,"\n", m,"\n", div,"\n", e,"\n", i, sep="")
+print(s, sb, m, div, e, i, sep="-")
